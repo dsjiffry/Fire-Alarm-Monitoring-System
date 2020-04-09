08e8a6c136abadacd880d29e2fb06abc069eaee0
Shehan <dsjiffry@gmail.com>
2020-04-09 15:07:20 +0530

    init rmi client

--- client/src/main/java/rmiClient/Main.java
+++ client/src/main/java/rmiClient/Main.java
@@ -1,31 +1,29 @@
 package rmiClient;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
 public class Main 
 {
 	public static void main(String[] args)
 	{
 		 
-
-		      System.setProperty("java.security.policy", "file:allowall.policy");
-
-		        MathService service = null;
-		        try {
-		            service = (MathService) Naming.lookup("//localhost/CalculatorService");
-
-		            System.out.println  ("Number of Clients : " + service.addClient());
-		            System.out.println  ("Add : " + service.add(2,2));
-		            System.out.println  ("Subtract : " + service.subtract(5,2));
-		            System.out.println  ("Multiply : " + service.multiply(2,6));
-
-		            System.out.println  ("Divide : " +  service.divide(4,2));
-		 
-		        } catch (NotBoundException ex) {
-		            System.err.println(ex.getMessage());
-		        } catch (MalformedURLException ex) {
-		            System.err.println(ex.getMessage());
-		        } catch (RemoteException ex) {
-		            System.err.println(ex.getMessage());
-		        }
-		    }
+	
+	  System.setProperty("java.security.policy", "file:allowall.policy");
+	
+	    ServerInterface service = null;
+	    try {
+	        service = (ServerInterface) Naming.lookup("//localhost/rmiServer");
+	 
+	        } catch (NotBoundException ex) {
+	            System.err.println(ex.getMessage());
+	        } catch (MalformedURLException ex) {
+	            System.err.println(ex.getMessage());
+	        } catch (RemoteException ex) {
+	            System.err.println(ex.getMessage());
+	        }
+	    }
 
 }
