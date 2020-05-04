fca79a5245b4ec91896ba9bfdb40d4e32ddb6476
Aqeel-Zeid <aqeelaqeelzeid@gmail.com>
2020-05-04 14:30:09 +0530

    Adjusted Sensor Services

--- Sensor_Service/routes.js
+++ Sensor_Service/routes.js
@@ -1,6 +1,6 @@
 const express = require("express")
 
-const {Kafka} = require("kafkajs")
+const { Kafka } = require("kafkajs")
 
 
 const Sensor = require('./Sensor')
@@ -8,45 +8,154 @@ const Sensor = require('./Sensor')
 const router = express.Router()
 
 
-async function addSensorChannel(sensorUID)
-{
+async function addSensorChannel(sensorUID) {
     const kafka = new Kafka({
         "clientId": "node-sensor-api",
-        "brokers" :["localhost:9092"]
-   })
+        "brokers": ["localhost:9092"]
+    })
 
     const admin = kafka.admin();
     console.log("Connecting.....")
     await admin.connect()
     console.log("Connected!")
-    
+
     await admin.createTopics({
         "topics": [{
-            "topic" : sensorUID,
-            "numPartitions" : 1
+            "topic": sensorUID,
+            "numPartitions": 1
         }]
     })
 
     console.log("Created Successfully!")
     await admin.disconnect();
-    
+
 }
 
+router
+    .get("/getSensorReading/:sensorUID",
+        async (req, res) => {
+            let sensorUID = req.params.sensorUID;
 
-router.delete("/deleteSensor" , async (req, res) => 
-{
+            let reading = 0
+
+            Sensor.exists({ sensorUID: sensorUID },
+                async function (err, result) {
+
+                    // console.log(err, "error")
+                    // console.log(result, "result")
+
+                    reading = "not found";
+
+                    if (result === false) {
+                        res.status(404)
+                        res.send({reading:reading});
+                    }
+                    else {
+
+                        const doc = await Sensor.findOne({ sensorUID: sensorUID });
+                        res.status(200);
+                        // reading = doc.reading;
+                        res.send({reading:doc.reading});
+                    }
+                })
+
+            
+
+        }
+    )
+
+
+router
+    .put("/updateSensorReading/:sensorUID",
+        async (req, res) => {
+
+            let sensorUID = req.params.sensorUID;
+            let reading = req.body.reading;
+
+            
+
+            Sensor.exists({ sensorUID: sensorUID }, async function (err, result) {
+
+               //console.log(`sensor exist ${err === false} result ${result === true}`)
+
+                if (result === false) {
+                    res.send({});
+                    res.status(404)
+                }
+                else {
+                    const doc = await Sensor.findOne({ sensorUID: sensorUID });
+                    
+                    //console.log(reading)
+                    
+                    doc.reading = parseInt(reading)
+
+                    await doc.save();
+
+                    const sensor = await Sensor.findOne({ sensorUID: req.params.sensorUID });
+                    
+                    res.send(sensor);
+                    res.status(200);
+                   
+                    //res.send(sensor);
+                }
+            })
+             
+            
+        }
+    )
+
+router
+    .put("/updateSensorStatus/:sensorUID",
+        async (req, res) => {
+
+            let sensorUID = req.params.sensorUID;
+            let status = req.query.status;
+
+            Sensor.exists({ sensorUID: sensorUID }, async function (err, result) {
+
+                // console.log(err, "error")
+                // console.log(result, "result")
+
+                if (result === false) {
+                    res.status(404)
+
+                }
+                else {
+                    // let doc = await Sensor.findOneAndUpdate({ sensorUID: req.body.sensorUID }, sensor)
+                    const doc = await Sensor.findOne({ sensorUID: sensorUID });
+                    doc.status = status
+
+                    await doc.save();
+
+                    res.status(200);
+                    //res.send(sensor);
+                }
+
+            })
+
+            const sensor = await Sensor.findOne({ sensorUID: req.body.sensorUID });
+            res.send(sensor);
+
+        }
+    )
+
+
+
+
+
+
+
+
+router.delete("/deleteSensor", async (req, res) => {
     let sensorUID = req.query.sensorUID;
 
-    Sensor.exists({sensorUID:sensorUID} , async (err, result) => 
-    {
-        if(result === true )
-        {
-            let sensor = await Sensor.deleteOne({sensorUID:sensorUID});
+    Sensor.exists({ sensorUID: sensorUID }, async (err, result) => {
+        if (result === true) {
+            let sensor = await Sensor.deleteOne({ sensorUID: sensorUID });
             await Sensor.remove(sensor);
             res.status(200).send("Successfully removes !")
         }
-        else
-        {
+        else {
             res.status(404).send("Sensor Not Found !")
         }
     })
@@ -59,62 +168,26 @@ router.get("/getAllSensors", async (req, res) => {
 
     res.send(sensors);
 
-} )
+})
 
 router.get("/getSensorsByUsername/:username", async (req, res) => {
 
     var username = req.params.username;
 
-    Sensor.exists({username : username} , async (err , result) => 
-    {
-        if(result === true)
-        {
-            let sensors = await Sensor.find({username: username });
+    Sensor.exists({ username: username }, async (err, result) => {
+        if (result === true) {
+            let sensors = await Sensor.find({ username: username });
             res.status(200);
             res.send(sensors);
         }
-        else
-        {
+        else {
             res.status(404)
             res.send([]);
         }
-    }) 
-   
-} )
-
-router.put("/updateSensorStatus/:sensorUID", async (req, res) => {
-    let sensorUID = req.params.sensorUID;
-    let status = req.query.status;
-
-
-    Sensor.exists({ sensorUID: sensorUID }, async function (err, result) {
-
-        console.log(err, "error")
-        console.log(result, "result")
-
-        if (result === false) {
-            res.status(404)
-
-        }
-        else {
-            // let doc = await Sensor.findOneAndUpdate({ sensorUID: req.body.sensorUID }, sensor)
-            const doc = await Sensor.findOne({ sensorUID: sensorUID });
-            doc.status = status
-
-            await doc.save();
-
-            res.status(200);
-            //res.send(sensor);
-
-        }
-        const sensor = await Sensor.findOne({ sensorUID: req.body.sensorUID });
-        res.send(sensor);
-
-
     })
 
-
 })
+
 
 
 router.put("/updateSensor", async (req, res) => {
@@ -131,16 +204,13 @@ router.put("/updateSensor", async (req, res) => {
 
     Sensor.exists({ sensorUID: req.body.sensorUID }, async function (err, result) {
 
-        console.log(err, "error")
-        console.log(result, "result")
-        
-        if(result === false)
-        {
+        // console.log(err, "error")
+        // console.log(result, "result")
+
+        if (result === false) {
             res.status(404)
-         
         }
-        else
-        {
+        else {
             // let doc = await Sensor.findOneAndUpdate({ sensorUID: req.body.sensorUID }, sensor)
             const doc = await Sensor.findOne({ sensorUID: req.body.sensorUID });
             doc.floor = req.body.floor;
@@ -154,8 +224,8 @@ router.put("/updateSensor", async (req, res) => {
         }
 
         res.send(sensor);
-        
-    
+
+
     })
 
 
@@ -171,20 +241,19 @@ router.post("/registerSensor", async (req, res) => {
             floor: req.body.floor,
             room: req.body.room,
             sensorType: req.body.sensorType,
-			status: "online"
+            status: "online",
+            reading: "Unread Sensor"
         })
 
     Sensor.exists({ sensorUID: req.body.sensorUID }, async function (err, result) {
 
-       
-        if(result === false)
-        {
+
+        if (result === false) {
             await sensor.save()
             addSensorChannel(req.body.sensorUID);
             res.status(201)
         }
-        else
-        {
+        else {
             res.status(409)
         }
 
@@ -194,6 +263,8 @@ router.post("/registerSensor", async (req, res) => {
 
 
 })
+
+
 
 
 module.exports = router
